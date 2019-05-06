cbc5cf8ea2ad3140646116a73a7521282b568a12
Adrián <adriannegrinmartin95@gmail.com>
2019-05-06 22:18:41 +0100

    IMG in edit item Done

--- src/pages/edit-stock/edit-stock.ts
+++ src/pages/edit-stock/edit-stock.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Loading, LoadingController,IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ProductDetailPage} from '../product-detail/product-detail';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {DbApiService} from '../../shared/db-api.service';
 import {ManageStockPage} from '../manage-stock/manage-stock';
+import { CloudinaryOptions, CloudinaryUploader} from 'ng2-cloudinary';
+import {environment} from '../../environments/environment';
 
 /**
  * Generated class for the EditStockPage page.
@@ -20,12 +22,25 @@ import {ManageStockPage} from '../manage-stock/manage-stock';
 export class EditStockPage {
   private item:any;
   productForm: FormGroup;
+  public_id       : string;
+  private productImg;
+  private loading : Loading;
+
+  uploader: CloudinaryUploader = new CloudinaryUploader(
+    new CloudinaryOptions({
+      cloudName: environment.cloudinary.cloud_name,
+      uploadPreset: environment.cloudinary.upload_preset
+    })
+  );
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-  public formBuilder: FormBuilder,
-  public dbapi: DbApiService) {
+              public formBuilder: FormBuilder,
+              public dbapi: DbApiService,
+              public loadingCtrl : LoadingController
+  ) {
     this.item = navParams.data;
     this.productForm = this.createMyForm();
+    this.productImg= this.item.img;
   }
 
   ionViewDidLoad() {
@@ -39,14 +54,40 @@ export class EditStockPage {
       price: ['', Validators.required],
     });
   }
+
+
   saveData(){
-  this.dbapi.pushItem(this.productForm.value.name,
-    this.productForm.value.description,
-    this.productForm.value.category,
-    this.item.id,
-    this.productForm.value.price
-  );
+    console.log("uploading", this.productForm);
+    this.loading = this.loadingCtrl.create();
+    this.loading.present();
+
+    if(this.uploader.queue.length){
+      this.uploader.uploadAll();
+      this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any): any => {
+        let res: any = JSON.parse(response);
+        console.log(res);
+        this.productImg = res.public_id;
+        this.pushNewUserData()
+      };
+    }else{
+      this.pushNewUserData();
+    }
   }
+
+
+  private pushNewUserData() {
+    this.dbapi.pushItem(
+      this.productForm.value.name,
+      this.productForm.value.description,
+      this.productForm.value.category.toString().split("\n").join("").replace(/\s/g, "").split(","),
+      this.item.id,
+      this.productForm.value.price,
+      this.productImg
+
+    );
+    this.loading.dismiss();
+  }
+
   backToManage() {
     this.navCtrl.pop();
 
