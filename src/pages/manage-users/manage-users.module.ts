495359b0a38ea3181179775ce7f0e8ebfbcf101e
AitorSDL <aitor.sdl@gmail.com>
2019-05-07 21:00:47 +0200

    Admin view done

--- src/pages/manage-users/manage-users.module.ts
+++ src/pages/manage-users/manage-users.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ManageUsersPage } from './manage-users';
+import {Ng2CloudinaryModule} from "ng2-cloudinary";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ManageUsersPage } from './manage-users';
   ],
   imports: [
     IonicPageModule.forChild(ManageUsersPage),
+    Ng2CloudinaryModule,
   ],
 })
 export class ManageUsersPageModule {}
