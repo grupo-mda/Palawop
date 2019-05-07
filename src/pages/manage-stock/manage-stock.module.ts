649fc25ff41a00ff5511fe7566e7164cab9358b9
AitorSDL <aitor.sdl@gmail.com>
2019-05-07 20:51:32 +0200

    Search Page and Category Page Added

--- src/pages/manage-stock/manage-stock.module.ts
+++ src/pages/manage-stock/manage-stock.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ManageStockPage } from './manage-stock';
+import {Ng2CloudinaryModule} from "ng2-cloudinary";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ManageStockPage } from './manage-stock';
   ],
   imports: [
     IonicPageModule.forChild(ManageStockPage),
+    Ng2CloudinaryModule,
   ],
 })
 export class ManageStockPageModule {}
